09b0e1346b1283176af53b5d20cc3e87c1485e4b
code-Harsh247 <justharsh2407@gmail.com>
2024-07-18 00:42:27 +0530

    Sidebar layout done

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -7,13 +7,14 @@ const Homelayout = ({ children }: { children: ReactNode }) => {
         <main className='relative'>
             <Navbar />
             <div className='flex'>
-            <Sidebar />
+                <Sidebar />
+                <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+                    <div className='w-full'>
+                        {children}
+                    </div>
+                </section>
             </div>
-            <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
-                <div className='w-full'>
-                    {children}
-                </div>
-            </section>
+
 
 
         </main>
