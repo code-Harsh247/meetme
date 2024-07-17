ffd4225649dc6936c9bb742ca82a855fd690a738
code-Harsh247 <justharsh2407@gmail.com>
2024-07-17 23:32:35 +0530

    making sidebar

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/Home.svg',
+    },
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg',
+    },
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/Video.svg',
+    },
+    {
+        label: 'PersonalRoom',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg',
+    },
+]
