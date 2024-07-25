a25627dc0f69b5acad2b84903c558a104649ccb3
code-Harsh247 <justharsh2407@gmail.com>
2024-07-25 20:26:36 +0530

    app

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,24 +1,28 @@
-import React, { ReactNode } from 'react'
-import Navbar from '@/components/ui/navbar'
-import Sidebar from '@/components/ui/sidebar'
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
-const Homelayout = ({ children }: { children: ReactNode }) => {
-    return (
-        <main className='relative'>
-            <Navbar />
-            <div className='flex'>
-                <Sidebar />
-                <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
-                    <div className='w-full'>
-                        {children}
-                    </div>
-                </section>
-            </div>
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 
+export const metadata: Metadata = {
+  title: 'YOOM',
+  description: 'A workspace for your team, powered by Stream Chat and Clerk.',
+};
 
+const RootLayout = ({ children }: Readonly<{children: ReactNode}>) => {
+  return (
+    <main className="relative">
+      <Navbar />
 
-        </main>
-    )
-}
+      <div className="flex">
+        <Sidebar />
+        
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
 
-export default Homelayout
+export default RootLayout;
